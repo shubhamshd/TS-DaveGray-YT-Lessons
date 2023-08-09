56df5a4dd5151e322e654c4f1e6cfc61dc33392d
Shubham Dubey <shubhamshd99@gmail.com>
2023-08-10 03:08:08 +0530

    lesson1 changes

---
+++ lesson1/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let user = 'shubham';
+console.log(user);
+let a = 3;
+let b = 2;
+let c = 4;
+console.log(a * b);
---
+++ lesson1/src/main.ts
@@ -0,0 +1,8 @@
+let user = 'shubham'
+console.log(user)
+
+let a:number = 3
+let b:number = 2
+let c:number = 4
+
+console.log(a*b)
