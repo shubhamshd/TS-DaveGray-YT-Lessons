22d0c191b4b1bcaa5e800f909379be963e58b471
Shubham Dubey <shubhamshd99@gmail.com>
2023-08-15 16:56:06 +0530

    type assertions

---
+++ lesson5/build/js/copyright.js
@@ -0,0 +1,18 @@
+"use strict";
+// Original JS code
+// const year = document.getElementById("year")
+// const thisYear = new Date().getFullYear()
+// year.setAttribute("datetime", thisYear)
+// year.textContent = thisYear
+// 1st variation
+// const year = document.getElementById("year")
+// const thisYear = new Date().getFullYear().toString()
+// if(year){
+//   year.setAttribute("datetime", thisYear)
+//   year.textContent = thisYear 
+// }
+// 2nd variation
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
---
+++ lesson5/build/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+let a = 'hello2';
+console.log(a);
+let b = a; //less specific
+console.log(b);
+let c = a; //more specific
+console.log(c);
+console.log(typeof c);
+// example of using type assertion when using ext libs with incomplete type definitions
+// mistakes can be made when using type assertions, so be careful!!!
+// import axios from 'axios';
+// // Making an HTTP GET request using axios
+// axios.get('https://api.example.com/data')
+// .then(response => {
+//     // The type of 'response' might not be well understood by TypeScript
+//     // Use type assertion to give TypeScript a hint about the type
+//     const data = response.data as { id: number; name: string; };
+//     // Now, TypeScript knows that 'data' has 'id' and 'name' properties
+//     console.log(data.id, data.name);
+// })
+// .catch(error => {
+//     console.error(error);
+// });
