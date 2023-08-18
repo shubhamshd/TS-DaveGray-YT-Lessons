6637fd7a380fb85917cbbcda0bc284dd1174a2ce
Shubham Dubey <shubhamshd99@gmail.com>
2023-08-18 18:07:34 +0530

    advance generics

--- lesson8/build/js/main.js
+++ lesson8/build/js/main.js
@@ -15,14 +15,15 @@ console.log(isObj('raj'));
 console.log(isObj(null));
 console.log(isObj([1, 'fds', false]));
 console.log(isObj({ name: 'raj', class: 'cs' }));
-const isTrue = (arg) => {
-    if (isObj(arg) && !Object.keys(arg).length) {
-        return { arg, is: false };
+// checks truthiness of an argument, returns false for falsy values such as false, 0, '', null, undefined, {}, []
+const isTrue = (value) => {
+    if (isObj(value) && !Object.keys(value).length) {
+        return { value, is: false };
     }
-    else if (Array.isArray(arg) && !arg.length) {
-        return { arg, is: false };
+    else if (Array.isArray(value) && !value.length) {
+        return { value, is: false };
     }
-    return { arg, is: !!arg };
+    return { value, is: !!value };
 };
 console.log(isTrue(true));
 console.log(isTrue(false));
@@ -36,3 +37,48 @@ console.log(isTrue({}));
 console.log(isTrue([]));
 console.log(isTrue(['true', 123]));
 console.log(isTrue({ name: 'fsd', place: 'fsd' }));
+// extend type
+const idCheck = (arg) => arg;
+console.log(idCheck({ id: 1, name: 'raj' }));
+// console.log(idCheck({name: 'raj'}))  // gives error due to absent id prop
+const getUserProps = (users, key) => {
+    return users.map(user => user[key]);
+};
+const userArray = [
+    {
+        id: 1,
+        name: 'raj',
+        email: 'raj@example.com'
+    },
+    {
+        id: 2,
+        name: 'mohit',
+        email: 'mohit@example.com'
+    },
+    {
+        id: 3,
+        name: 'rohit',
+        email: 'rohit@example.com'
+    }
+];
+console.log(getUserProps(userArray, 'name'));
+console.log(getUserProps(userArray, 'email'));
+console.log(getUserProps(userArray, 'id'));
+/////////////////////////////////////////Generic in Class
+class State {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(newState) {
+        this.state = newState;
+    }
+}
+const myState = new State('raj');
+// const myState = new State<number>('raj') // this gives error
+const newState = new State(['sdf', 12, true]);
+console.log(newState.data);
+newState.data = [324, false, 'raj'];
+console.log(newState.data);
--- lesson8/src/main.ts
+++ lesson8/src/main.ts
@@ -18,15 +18,21 @@ console.log(isObj(null))
 console.log(isObj([1, 'fds', false]))
 console.log(isObj({name: 'raj', class: 'cs'}))
 
+// type defintion for return type of isTrue() function
+type boolcheck<T> = {
+  value: T,
+  is: boolean
+}
 
-const isTrue = <T>(arg: T): {arg: T, is: boolean} => {
-  if(isObj(arg) && !Object.keys(arg as keyof T).length){
-    return {arg, is: false}
+// checks truthiness of an argument, returns false for falsy values such as false, 0, '', null, undefined, {}, []
+const isTrue = <T>(value: T): boolcheck<T> => {
+  if(isObj(value) && !Object.keys(value as keyof T).length){
+    return {value, is: false}
   }
-  else if (Array.isArray(arg) && !arg.length){
-    return {arg, is: false}
+  else if (Array.isArray(value) && !value.length){
+    return {value, is: false}
   }
-  return {arg, is: !!arg}
+  return {value, is: !!value}
 }
 
 console.log(isTrue(true))
@@ -41,3 +47,69 @@ console.log(isTrue({}))
 console.log(isTrue([]))
 console.log(isTrue(['true', 123]))
 console.log(isTrue({name: 'fsd', place: 'fsd'}))
+
+////////////////////////////////////////////////
+
+type hasId = {
+  id: number
+}
+
+// extend type
+const idCheck = <T extends hasId>(arg: T): T => arg
+
+console.log(idCheck({id: 1, name: 'raj'}))
+// console.log(idCheck({name: 'raj'}))  // gives error due to absent id prop
+
+const getUserProps = <T extends hasId, K extends keyof T>(users: T[], key: K): T[K][] => {
+  return users.map(user => user[key])
+}
+
+const userArray = [
+  {
+    id: 1,
+    name: 'raj',
+    email: 'raj@example.com'
+  },
+  {
+    id: 2,
+    name: 'mohit',
+    email: 'mohit@example.com'
+  },
+  {
+    id: 3,
+    name: 'rohit',
+    email: 'rohit@example.com'
+  }
+]
+
+console.log(getUserProps(userArray, 'name'))
+console.log(getUserProps(userArray, 'email'))
+console.log(getUserProps(userArray, 'id'))
+
+
+/////////////////////////////////////////Generic in Class
+
+class State<T> {
+  data: T
+
+  constructor(value: T){
+    this.data = value
+  }
+
+  get state(): T {
+    return this.data
+  }
+
+  set state(newState: T) {
+    this.state = newState
+  }
+}
+
+const myState = new State('raj')
+// const myState = new State<number>('raj') // this gives error
+
+const newState = new State<(string | number | boolean)[]>(['sdf', 12, true])
+console.log(newState.data)
+
+newState.data = [324, false, 'raj']
+console.log(newState.data)
